7be8132180cb4ce02519b297d68f5a3fb2cc8ee6
harshGitprofile <hraj09590@gmail.com>
2026-09-26 00:08:14 +0530

    Add js file

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("hlo javascript");
+console.clear()
+let value = 123;
+let val = "wer";
+console.log(typeof val);
+let abc = {
+    name : "harsh",
+    age : 13,
+    address : {
+        state : "BH",
+        PIN : 123456
+    },
+    skills : {
+        frontend : ["abc","def"],
+        backend :["def"],
+        db : ["234"]
+    },
+};
+console.log(typeof abc);
